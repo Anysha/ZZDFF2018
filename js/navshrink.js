e443d9954097caf3af55ac9c1e481df2ab19c02c
Anysha <anyshajain@yahoo.com>
2018-08-19 13:18:49 +0530

    ZZDFF18

---
+++ js/navshrink.js
@@ -0,0 +1,14 @@
+
+$(Document).ready(function() {
+    
+    $(window).scroll(function() { 
+        
+        if($(document).scrollTop() > 10) {
+        
+            $('.nav').addClass('change'); 
+    }
+    else { 
+        $('.nav').removeClass('change'); 
+    }
+});
+});
